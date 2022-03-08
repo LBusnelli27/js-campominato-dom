51adeaa5817b21395ee86fc56baaf3179f13ac2e
Busna27 <lucabusnelli2001@gmail.com>
2022-03-08 17:35:03 +0100

    refactoring name js + fix tag html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // ********** FUNCTION **********
 
 /**
- * This function create a div with a class
+ * This function create a div with a class my-col
  * 
  * @returns A div with class my-col
  */
-function createGridSquare() {
+function createDivCol() {
     const currentElement = document.createElement('div')
     currentElement.classList.add('my-col')
     
@@ -13,18 +13,56 @@ function createGridSquare() {
 }
 
 /**
+ * This function generate unique number so it don't generate a number twice
+ * 
+ * @param {*} numsBlacklist The blacklist of numbers to check
+ * @param {*} min Minimum value
+ * @param {*} max Maximum value
+ * @returns A random generated integer which is not present in the blacklist
+ */
+function generateUniqueRandomNumber( numsBlacklist, min, max){
+    let check = false;
+    let randomInt;
+
+    while ( !check ){
+        randomInt  =  Math.floor(Math.random() * ((max + 1) - min) + min);
+        if ( !numsBlacklist.includes(randomInt)  ){
+            check = true;
+        }
+    }
+
+    return randomInt;
+}
+
+/**
+ * 
+ * @param {*} bombs Number of bombs
+ * @param {*} numberMaxCells Total namber of cells
+ * @returns Array of numbers (bombs)
+ */
+function genBombs(bombs, numberMaxCells) {
+    const bombsArray = [];
+
+    for(let i = 0; i < bombs; i++) {
+        bombsArray.push(generateUniqueRandomNumber(bombsArray, 1, numberMaxCells));
+    }
+
+    return bombsArray;
+}
+
+/**
  * This function create n. boxes and add an eventListener
  * 
  * @param {*} nBox Number of boxes generated
  */
-function createCol(nBox) {
+function createBoxAddBombs(nBox) {
     const bombs = genBombs(16, nBox);
     console.log(bombs);
-
+    
     let points = 0;
 
     for(let i = 0; i < nBox; i++) {
-        const currentSquare = createGridSquare();
+        const currentSquare = createDivCol();
         currentSquare.innerHTML = `${i + 1}`;
 
         
@@ -51,44 +89,6 @@ function resetNewGame() {
     endGamePhrase.innerHTML = "";
 }
 
-/**
- * 
- * @param {*} bombs Number of bombs
- * @param {*} numberMaxCells Total namber of cells
- * @returns Array of numbers (bombs)
- */
-function genBombs(bombs, numberMaxCells) {
-    const bombsArray = [];
-
-    for(let i = 0; i < bombs; i++) {
-        bombsArray.push(generateUniqueRandomNumber(bombsArray, 1, numberMaxCells));
-    }
-
-    return bombsArray;
-}
-
-/**
- * This function generate unique number so it don't generate a number twice
- * 
- * @param {*} numsBlacklist The blacklist of numbers to check
- * @param {*} min Minimum value
- * @param {*} max Maximum value
- * @returns A random generated integer which is not present in the blacklist
- */
-function generateUniqueRandomNumber( numsBlacklist, min, max){
-    let check = false;
-    let randomInt;
-
-    while ( !check ){
-        randomInt  =  Math.floor(Math.random() * ((max + 1) - min) + min);
-        if ( !numsBlacklist.includes(randomInt)  ){
-            check = true;
-        }
-    }
-
-    return randomInt;
-}
-
 // ********** END FUNCTION **********
 
 
@@ -102,11 +102,11 @@ playBtn.addEventListener('click', function() {
 
     const difficultyChoise = document.getElementById('inputGroupSelect');
     if (difficultyChoise.value == 1) {
-        createCol(100);
+        createBoxAddBombs(100);
     } else if (difficultyChoise.value == 2) {
-        createCol(80);
+        createBoxAddBombs(80);
     } else if (difficultyChoise.value == 3) {
-        createCol(50);
+        createBoxAddBombs(50);
     } else {
         console.warn('Non hai creato un bel nulla!!!')
     }
